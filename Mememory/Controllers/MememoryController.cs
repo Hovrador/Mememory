da4f35da383c9b6a849d07189d41b7e7fff126d9
Hovrador <hovrador@gmail.com>
2014-11-22 22:24:26 -0600

    Memory Wireframe
    Created wireframe of basic site. Made placeholder MVC objects to test
    code tomorrow.

---
+++ Mememory/Controllers/MememoryController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mememory.Controllers
+{
+    public class MememoryController : Controller
+    {
+        // GET: Mememory
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Mememory/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Mememory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mememory/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Mememory/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Mememory/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Mememory/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Mememory/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
