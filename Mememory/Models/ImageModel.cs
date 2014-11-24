257063eebad5c75f2f134171a00201190bd3ac23
Hovrador <hovrador@gmail.com>
2014-11-24 14:54:27 -0600

    Image upload models
    Added models to access xml data and build list of images.

---
+++ Mememory/Models/ImageModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Mememory.Models
+{
+    public class ImageModel : List<UploadModel>
+    {
+        public ImageModel()
+        {
+            string imagesDir = HttpContext.Current.Server.MapPath("~/Images/");
+            XDocument imagesMetaData = XDocument.Load(imagesDir + @"/ImagesMetaData.xml");
+            var images = from image in imagesMetaData.Descendants("image")
+                         select new UploadModel(image.Element("title").Value,
+                         image.Element("url").Value,
+                         image.Element("description").Value,
+                         image.Element("genre").Value);
+            this.AddRange(images.ToList<UploadModel>());
+        }
+    }
+}
--- Mememory/Models/UploadModel.cs
+++ Mememory/Models/UploadModel.cs
@@ -7,6 +7,13 @@ namespace Mememory.Models
 {
     public class UploadModel
     {
+        public UploadModel(string t, string u, string d, string g)
+        {
+            title = t;
+            url = u;
+            description = d;
+            genre = g;
+        }
         public string title { get; set; }
         public string url { get; set; }
         public string description { get; set; }
